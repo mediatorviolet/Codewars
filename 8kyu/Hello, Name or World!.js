a09cf4e1eae98c928093d63cbfec21b175e8645a
Antoine SCHILT <antoineschilt@gmail.com>
2021-08-03 13:14:52 +0400

    8kyu - Hello, Name or World!

---
+++ 8kyu/Hello, Name or World!.js
@@ -0,0 +1,26 @@
+/**
+ * 8kyu - Hello, Name or World!
+ *
+ * Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
+ *
+ * Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+ *
+ * Examples:
+ *
+ * hello "john"   => "Hello, John!"
+ * hello "aliCE"  => "Hello, Alice!"
+ * hello          => "Hello, World!" # name not given
+ * hello ""       => "Hello, World!" # name is an empty String
+ *
+ */
+
+const hello = (name) => {
+  return typeof name === "string" && name !== ""
+    ? `Hello, ${name.slice(0, 1).toUpperCase()}${name.slice(1).toLowerCase()}!`
+    : "Hello, World!";
+};
+
+console.log(hello("johN")); // "Hello, John!"
+console.log(hello("alice")); // "Hello, Alice!"
+console.log(hello()); // "Hello, World!"
+console.log(hello("")); // "Hello, World!"
