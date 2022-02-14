796017d76883a2a09fa9e2c67fd513c6fedaaff0
Antoine SCHILT <antoineschilt@gmail.com>
2022-02-14 09:53:26 +0400

    6kyu - Find the missing letter

---
+++ 6kyu/Find the missing letter.js
@@ -0,0 +1,44 @@
+/**
+ * 6kyu - Find the missing letter
+ *
+ * Find the missing letter
+ *
+ * Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+ *
+ * You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+ * The array will always contain letters in only one case.
+ *
+ * Example:
+ *
+ * ['a','b','c','d','f'] -> 'e' ['O','Q','R','S'] -> 'P'
+ *
+ * ["a","b","c","d","f"] -> "e"
+ * ["O","Q","R","S"] -> "P"
+ *
+ * (Use the English alphabet with 26 letters!)
+ *
+ * Have fun coding it and please don't forget to vote and rank this kata! :-)
+ *
+ * I have also created other katas. Take a look if you enjoyed this kata!
+ */
+
+const findMissingLetter = (array) => {
+    const ALPHABET = 'abcdefghijklmnopqrstuvwxyz'.split('')
+    let isUpper = false
+
+    let alphaFirstIndex = ALPHABET.findIndex((el) => el === array[0])
+    if (alphaFirstIndex === -1) {
+        alphaFirstIndex = ALPHABET.findIndex((el) => el === array[0].toLowerCase())
+        isUpper = true
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].toLowerCase() !== ALPHABET[alphaFirstIndex + i]) {
+            console.log(isUpper ? ALPHABET[alphaFirstIndex + i].toUpperCase() : ALPHABET[alphaFirstIndex + i])
+            return isUpper ? ALPHABET[alphaFirstIndex + i].toUpperCase() : ALPHABET[alphaFirstIndex + i]
+        }
+    }
+}
+
+findMissingLetter(['a', 'b', 'c', 'd', 'f']) // 'e'
+findMissingLetter(['O', 'Q', 'R', 'S']) // 'P'
